f42327d8614930e828994a687470cfb64e0c6650
mashU2BITs <117328325+mashU2BITs@users.noreply.github.com>
2022-11-02 19:40:22 -0700

    second project

---
+++ CalcOperations/Class1.cs
@@ -0,0 +1,25 @@
+﻿namespace CalcOperations
+{
+    public class CalcOps
+    {
+        public  float addition(float valA, float valB)
+        {
+            return valA + valB;
+        }
+
+        public  float subtraction(float valA, float valB)
+        {
+            return valA - valB;
+        }
+
+        public  float multiplication(float valA, float valB)
+        {
+            return valA * valB;
+        }
+
+        public  float division(float valA, float valB)
+        {
+            return valA / valB;
+        }
+    }
+}
